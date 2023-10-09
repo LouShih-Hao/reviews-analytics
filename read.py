e7bf9a8e4143652b0672736ad939a047f15f2053
Shih-Hao Lou <shihhao.lou@hotmail.com>
2023-10-09 12:03:56 +0800

    calculate average length of reviews

--- read.py
+++ read.py
@@ -4,6 +4,11 @@ with open('reviews.txt', 'r') as f:
 		data.append(line)
 		if len(data) % 1000 == 0:
 			print(len(data)) # 讀取檔案的過程中，印出len(data)才知道讀取進度
-print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print(data[0])
+# 算留言平均長度
+total_len = 0 
+
+for d in data:
+	total_len += len(d)
+print('這', len(data), '筆資料，其平均長度為：', total_len / len(data), '字元')	
