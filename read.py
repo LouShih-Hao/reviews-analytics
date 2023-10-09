c44bec2f4cf735cefdbf72b1829e5c8339a17b1b
Shih-Hao Lou <shihhao.lou@hotmail.com>
2023-10-09 12:24:35 +0800

    add filtering function for specific vocabulary

--- read.py
+++ read.py
@@ -17,4 +17,12 @@ new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
-print('一共有', len(new), '筆留言長度小於100')	
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言包含good')
+print(good[0])	
